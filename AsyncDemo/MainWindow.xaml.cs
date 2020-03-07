c290125f253caa3b2f442e2081c3262575ae6587
Zach Baird <zdb1994@yahoo.com>
2020-03-06 21:59:53 -0500

    Add project files.

---
+++ AsyncDemo/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AsyncDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void executeSync_Click(object sender, RoutedEventArgs e)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            RunDownloadSync();
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+
+            resultsWindow.Text += $"Total execution time: { elapsedMs }";
+        }
+
+        private void executeAsync_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private List<string> PrepData()
+        {
+            List<string> output = new List<string>();
+
+            resultsWindow.Text = "";
+
+            output.Add("https://www.yahoo.com");
+            output.Add("https://www.google.com");
+            output.Add("https://www.microsoft.com");
+            output.Add("https://www.cnn.com");
+            output.Add("https://www.codeproject.com");
+            output.Add("https://www.stackoverflow.com");
+
+            return output;
+        }
+
+        private void RunDownloadSync()
+        {
+            List<string> websites = PrepData();
+
+            foreach (var site in websites)
+            {
+                WebSiteDataModel results = DownloadWebSite(site);
+                ReportWebSiteInfo(results);
+            }
+        }
+
+        private WebSiteDataModel DownloadWebSite(string webSiteUrl)
+        {
+            WebSiteDataModel output = new WebSiteDataModel();
+            WebClient client = new WebClient();
+
+            output.WebSiteUrl = webSiteUrl;
+            output.WebSiteData = client.DownloadString(webSiteUrl);
+
+            return output;
+        }
+
+        private void ReportWebSiteInfo(WebSiteDataModel data)
+        {
+            resultsWindow.Text += $"{ data.WebSiteUrl } downloaded: { data.WebSiteData.Length } characters long. { Environment.NewLine }";
+        }
+    }
+}
---
+++ AsyncDemo/WebSiteDataModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AsyncDemo
+{
+    public class WebSiteDataModel
+    {
+        public string WebSiteUrl { get; set; } = "";
+        public string WebSiteData { get; set; } = "";
+    }
+}
